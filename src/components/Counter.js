e2d11bb0eec57bc763fc14c56d51339b4df67cf6
SaiPrasadRajuThangella <143223133+SaiPrasadRajuThangella@users.noreply.github.com>
2024-08-25 18:42:03 +0000

    added solution

---
+++ src/components/Counter.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    console.log(this);
+  }
+
+  render() {
+    const incrementCount = () => {
+      const curr = this.state.count;
+      this.setState({ count: 1 + curr });
+    };
+    return (
+      <>
+        <p>Button clicked {this.state.count} times</p>
+        <button onClick={incrementCount}>Click me</button>
+      </>
+    );
+  }
+}
+
+export default Counter;
